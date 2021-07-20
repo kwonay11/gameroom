d3a1b794ccc59645be8dad0b0f15eb4e32a947e1
bomin <rlaqhals0134@naver.com>
2021-07-21 02:36:20 +0900

    [S05P12C104-59] feat/Login : Add 로그인 페이지 UI 구현 완료

--- frontend/src/views/Accounts/Login.vue
+++ frontend/src/views/Accounts/Login.vue
@@ -1,36 +1,23 @@
 <template>
   <div>        
-      <div class="v194_2">
-        <div class="v194_4">
-          <div class="v194_6">
-            <div class="v194_7"></div>
-            <div class="name"></div>
-            <div class="name"></div>
-            <div class="name"></div>
-          </div>
-          <div class="v194_12">
-            <div class="v194_13"></div>
-            <div class="v194_14"></div>
-            <span class="v194_15">ID</span>
-          </div>
-          <div class="v194_17">
-            <div class="v194_18"></div>
-            <div class="v194_19"></div>
-            <span class="v194_20">Password</span>
-          </div>
-          <div class="v194_22">
-            <div class="v194_23"></div>
-            <div class="v194_24"></div>
-          </div>
-          <div class="v194_25">
-            <div class="v194_26"></div>
-            <span class="v194_27">GO TO SIGN UP</span>
-          </div>
-        </div>
+    <div class="login_box">
+      <div class="circle"></div>
+      <div class="id_pw">
+        <div class="id_left"></div>
+        <div class="id_right"></div>
       </div>
-    
- </div>
-
+      <div class="id_pw">
+        <div class="pw_left"></div>
+        <div class="pw_right"></div>
+      </div>
+      <div class="signup_box">
+        <router-link :to="{ name: 'MainPage' }" class='signup_text'>Login</router-link>
+      </div>
+      <div class="signup_box">
+        <router-link :to="{ name: 'Signup' }" class='signup_text'>GO TO SIGN UP</router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -42,246 +29,84 @@ export default {
 
 <style >
 
+
+
 * {
   box-sizing: border-box;
 }
 body {
   font-size: 14px;
 }
-
-.v194_2 {
-  width: 100%;
-  height: 1024px;
-  /* background: rgba(247,247,255,0); */
-  opacity: 1;
-  /* 고칠곳 */
-  /* position: absolute; */
-  margin: 0 auto;
-  border-top-right-radius: 20px;
-  border-bottom-left-radius: 20px;
-  overflow: hidden;
-}
-.v194_4 {
-  width: 928px;
-  height: 872px;
-  /* background: url("../images/v194_4.png"); */
-  /* background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;*/
+.login_box {
+  width: 600px;
+  height: 62vh;
+  border-radius: 15px;
+  backdrop-filter: blur(10px);
+  background: rgba(101, 51, 126, 0.6);
   position: relative;
   margin: 0 auto;
-  overflow: hidden;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content:space-around;
+  padding: 5px 0 5px 0;
+}
 
-  /* display: flex;           /* establish flex container */
-  flex-direction: column;  /* make main axis vertical */
-  justify-content: center; /* center items vertically, in this case */
-  align-items: center;     /* center items horizontally, in this case */
-  border: 1px grey solid; 
-}
-.v194_6 {
-  width: 484px;
-  height: 629px;
-  /* background: rgba(255,255,255,1); */
-  opacity: 1;
-  position: absolute;
-  top: 264px;
-  left: 225px;
-  overflow: hidden;
-}
-.v194_7 {
+.circle {
   width: 153px;
   height: 152px;
-  background: rgba(68,62,62,0.10000000149011612);
-  opacity: 1;
-  position: absolute;
-  top: 0px;
-  left: 163px;
+  background: rgba(219, 214, 214, 0.1);
   border-radius: 50%;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
-.name {
-  color: #fff;
-}
-.name {
-  color: #fff;
-}
-.name {
-  color: #fff;
-}
-.v194_12 {
-  width: 515px;
+
+.id_pw {
+  width: 482px;
   height: 82px;
-  /* background: url("../images/v194_12.png"); */
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 433px;
-  left: 212px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
-.v194_13 {
-  width: 405px;
-  height: 68px;
-  background: rgba(219,219,219,0.6000000238418579);
-  opacity: 1;
-  position: absolute;
-  top: 8px;
-  left: 92px;
-  border: 1px solid rgba(219,219,219,0);
-  border-top-right-radius: 20px;
-  border-bottom-right-radius: 20px;
-  overflow: hidden;
-}
-.v194_14 {
+.id_left {
+  
   width: 77px;
   height: 68px;
   background: rgba(49,46,46,0.5);
-  opacity: 1;
-  position: absolute;
-  top: 8px;
-  left: 15px;
-  border: 1px solid rgba(219,219,219,0.6000000238418579);
   border-top-left-radius: 20px;
   border-bottom-left-radius: 20px;
-  overflow: hidden;
 }
-.v194_15 {
-  width: 159px;
-  color: rgba(0,0,0,1);
-  position: absolute;
-  top: 27px;
-  left: 108px;
-  font-family: Roboto;
-  font-weight: Regular;
-  font-size: 30px;
-  opacity: 1;
-  text-align: left;
-}
-.v194_17 {
-  width: 515px;
-  height: 82px;
-  /* background: url("../images/v194_17.png"); */
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 533px;
-  left: 212px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  overflow: hidden;
-}
-.v194_18 {
+.id_right {
   width: 405px;
   height: 68px;
   background: rgba(219,219,219,0.6000000238418579);
-  opacity: 1;
-  position: absolute;
-  top: 8px;
-  left: 92px;
-  border: 1px solid rgba(219,219,219,0);
   border-top-right-radius: 20px;
   border-bottom-right-radius: 20px;
-  overflow: hidden;
 }
-.v194_19 {
+.pw_left {
   width: 77px;
   height: 68px;
-  background: rgba(39,34,34,0.5400000214576721);
-  opacity: 1;
-  position: absolute;
-  top: 8px;
-  left: 15px;
-  border: 1px solid rgba(219,219,219,0.6000000238418579);
+  background: rgba(49,46,46,0.5);
   border-top-left-radius: 20px;
   border-bottom-left-radius: 20px;
-  overflow: hidden;
 }
-.v194_20 {
-  width: 159px;
-  color: rgba(0,0,0,1);
-  position: absolute;
-  top: 24px;
-  left: 111px;
-  font-family: Roboto;
-  font-weight: Regular;
-  font-size: 30px;
-  opacity: 1;
-  text-align: left;
+.pw_right {
+  width: 405px;
+  height: 68px;
+  background: rgba(219,219,219,0.6000000238418579);
+  border-top-right-radius: 20px;
+  border-bottom-right-radius: 20px;
 }
-.v194_22 {
-  width: 513px;
-  height: 97px;
-  /* background: url("../images/v194_22.png"); */
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 633px;
-  left: 212px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  overflow: hidden;
+.signup_box {
+ width: 482px;
+ height: 65px;
+ background: rgba(49,46,46,0.5);
+ padding-top: 10px;
 }
-.v194_23 {
-  width: 482px;
-  height: 65px;
-  background: rgba(73,44,90,1);
-  opacity: 1;
-  position: absolute;
-  top: 13px;
-  left: 15px;
-  border: 1px solid rgba(219,219,219,1);
+.signup_text {
+ color: rgba(255,255,255,1);
+ font-family: Roboto;
+ font-size: 30px;
+ text-decoration: none;
 }
-.v194_24 {
-  width: 80px;
-  height: 21px;
-  background: rgba(255,255,255,1);
-  opacity: 1;
-  position: absolute;
-  top: 35px;
-  left: 209px;
-}
-.v194_25 {
-  width: 513px;
-  height: 97px;
-  /* background: url("../images/v194_25.png"); */
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 730px;
-  left: 212px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  overflow: hidden;
-}
-.v194_26 {
-  width: 482px;
-  height: 65px;
-  background: rgba(73,44,90,1);
-  opacity: 1;
-  position: absolute;
-  top: 13px;
-  left: 15px;
-  border: 1px solid rgba(219,219,219,1);
-}
-.v194_27 {
-  width: 229px;
-  color: rgba(255,255,255,1);
-  position: absolute;
-  top: 32px;
-  left: 143px;
-  font-family: Roboto;
-  font-weight: Regular;
-  font-size: 30px;
-  opacity: 1;
-  text-align: left;
-}
-
-
 </style>
