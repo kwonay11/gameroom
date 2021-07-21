a933afcab116eba4daa4011ae654ac8889bd5ae8
bomin <rlaqhals0134@naver.com>
2021-07-21 11:11:24 +0900

    [S05P12C104-66] feat/feature_Fe_Search : Add 검색 페이지 추가

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,16 +1,61 @@
 <template>
   <div>
-    검색
+    <!-- <div class="v53_353"> -->
+      <div class="v53_341">
+      <div class="v53_342"></div>
+      </div>
+      <!-- </div> -->
   </div>
 </template>
 
 <script>
 export default {
      name: "Search",
-
 }
 </script>
 
 <style>
+	* {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v53_353 {
+  width: 80%;
+  height: 189px;
+  position: relative;
+  margin: auto;
+  border: solid 1px
+
+}
+.v53_341 {
+  width: 80%;
+  height: 120px;
+  background: rgba(19,17,150,0.4000000059604645);
+
+  position: relative;
+  margin: auto;
+  /* border: solid 1px */
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  /* overflow: hidden; */
+}
+.v53_342 {
+  width: 95%;
+  height: 85px;
+  background: rgba(253,244,244,1);
+  margin: 20px;
+  position: absolute;
+
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  /* overflow: hidden; */
+}
+
 
 </style>
