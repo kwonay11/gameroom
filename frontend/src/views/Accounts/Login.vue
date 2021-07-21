9fdfa0a92a41ba7f4fef53f3d38c64ebe5881843
yebeen <kwonay11@naver.com>
2021-07-21 23:26:49 +0900

    [] feat/signup : Add 로그인,회원가입 디자인폼 완성

--- frontend/src/views/Accounts/Login.vue
+++ frontend/src/views/Accounts/Login.vue
@@ -1,14 +1,14 @@
 <template>
   <div>        
     <div class="login_box">
-      <div class="circle"></div>
+      <div class="circle"><img class="card__image" src="@/assets/user.png" alt="login"/></div>
       <div class="id_pw">
-        <div class="id_left"></div>
-        <div class="id_right"></div>
+        <div class="id_left"><img class="left_image" src="@/assets/profile.png" alt="user"/></div>
+        <div class="id_right"><input id="id" class="card__input" placeholder="ID" style="height:66px" type="text" /></div>
       </div>
       <div class="id_pw">
-        <div class="pw_left"></div>
-        <div class="pw_right"></div>
+        <div class="pw_left"><img class="left_image" src="@/assets/password.png" alt="pw"/></div>
+        <div class="pw_right"><input id="password" class="card__input" placeholder="Password" style="height:66px" type="password" /></div>
       </div>
       <div class="signup_box">
         <router-link :to="{ name: 'MainPage' }" class='signup_text'>Login</router-link>
@@ -31,15 +31,11 @@ export default {
 
 
 
-* {
-  box-sizing: border-box;
-}
-body {
-  font-size: 14px;
-}
+
+
 .login_box {
-  width: 600px;
-  height: 58vh;
+  width: 35%;
+  height:58vh;
   border-radius: 15px;
   backdrop-filter: blur(10px);
   background: rgba(101, 51, 126, 0.6);
@@ -51,6 +47,8 @@ body {
   align-items: center;
   justify-content:space-around;
   padding: 10px 0 10px 0;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  
 }
 
 .circle {
@@ -75,6 +73,8 @@ body {
   background: rgba(49,46,46,0.5);
   border-top-left-radius: 20px;
   border-bottom-left-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  
 }
 .id_right {
   width: 405px;
@@ -82,6 +82,7 @@ body {
   background: rgba(219,219,219,0.6000000238418579);
   border-top-right-radius: 20px;
   border-bottom-right-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 .pw_left {
   width: 77px;
@@ -89,6 +90,7 @@ body {
   background: rgba(49,46,46,0.5);
   border-top-left-radius: 20px;
   border-bottom-left-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 .pw_right {
   width: 405px;
@@ -96,12 +98,15 @@ body {
   background: rgba(219,219,219,0.6000000238418579);
   border-top-right-radius: 20px;
   border-bottom-right-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 .signup_box {
  width: 482px;
  height: 65px;
  background: rgba(49,46,46,0.5);
  padding-top: 10px;
+ box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+ border-radius: 20px;
 }
 .signup_text {
  color: rgba(255,255,255,1);
@@ -109,4 +114,32 @@ body {
  font-size: 30px;
  text-decoration: none;
 }
+
+.left_image {
+  max-width: 60%;
+  padding-top: 10px;
+}
+
+.card__image {
+  border-radius: 50%;
+  max-width: 100%;
+}
+.card__input {
+  border-radius: 20px;
+  box-sizing: border-box;
+  color: #000000;
+  display: block;
+  font-size: 1.5rem;
+  letter-spacing: 0.15rem;
+  padding: 10px;
+  width: 100%;
+}
+.card__input:focus, .card__input:active {
+  background: rgba(254, 254, 254, 0.08);
+  
+  outline: none;
+}
+
+
 </style>
+
--- frontend/src/views/Accounts/Signup.vue
+++ frontend/src/views/Accounts/Signup.vue
@@ -1,16 +1,125 @@
 <template>
-  <div>
-    회원가입
+  <div>       
+    <div class="signup">
+      <div class="circle"><img class="card__image" src="@/assets/follower (1).png" alt="login"/></div>
+      <div class="id_pw">
+        <div class="left"><img class="left_image" src="@/assets/profile.png" alt="id"/></div>
+        <div class="right"><input id="id" class="card__input" placeholder="ID" style="height:66px" type="text" /></div>
+      </div>
+      <div class="id_pw">
+        <div class="left"><img class="left_image" src="@/assets/nickname.png" alt="nickname"/></div>
+        <div class="right"><input id="nickname" class="card__input" placeholder="Nickname" style="height:66px" type="text" /></div>
+      </div>
+      <div class="id_pw">
+        <div class="left"><img class="left_image" src="@/assets/lock.png" alt="pw"/></div>
+        <div class="right"><input id="password" class="card__input" placeholder="Password" style="height:66px" type="password" /></div>
+      </div>
+      <div class="id_pw">
+        <div class="left"><img class="left_image" src="@/assets/lock2.png" alt="pw_check"/></div>
+        <div class="right"><input id="password" class="card__input" placeholder="Password Check" style="height:66px" type="password" /></div>
+      </div>
+      <div class="signup_box">
+        <router-link :to="{ name: 'Login' }" class='signup_text'>SIGN UP</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-     name: "Signup",
+     name: "Login",
 
 }
 </script>
 
-<style>
+<style >
 
+
+
+
+.signup {
+  width: 35%;
+  height: 62vh;
+  border-radius: 15px;
+  backdrop-filter: blur(10px);
+  background: rgba(101, 51, 126, 0.6);
+  position: relative;
+  margin: 0 auto;
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content:space-around;
+  padding: 10px 0 10px 0;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.circle {
+  width: 153px;
+  height: 152px;
+  background: rgba(219, 214, 214, 0.1);
+  border-radius: 50%;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  margin-bottom : 20px;
+  margin-top : 20px;
+}
+
+.id_pw {
+  width: 482px;
+  height: 82px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom : 20px;
+  
+}
+.left {
+  width: 77px;
+  height: 68px;
+  background: rgba(49,46,46,0.5);
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+.right {
+  width: 405px;
+  height: 68px;
+  background: rgba(219,219,219,0.6000000238418579);
+  border-top-right-radius: 20px;
+  border-bottom-right-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+.signup_box {
+ width: 482px;
+ height: 65px;
+ background: rgba(49,46,46,0.5);
+ padding-top: 10px;
+ box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+.signup_text {
+ color: rgba(255,255,255,1);
+ font-family: Roboto;
+ font-size: 30px;
+ text-decoration: none;
+}
+
+.card__image {
+  border-radius: 50%;
+  max-width: 100%;
+}
+.card__input {
+  border-radius: 20px;
+  box-sizing: border-box;
+  color: #000000;
+  display: block;
+  font-size: 1.5rem;
+  letter-spacing: 0.15rem;
+  padding: 10px;
+  width: 100%;
+}
+.card__input:focus, .card__input:active {
+  background: rgba(254, 254, 254, 0.08);
+  
+  outline: none;
+}
 </style>
