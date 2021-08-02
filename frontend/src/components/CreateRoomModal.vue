ed951ad618ba1676a6dc85e9efd1a07e53c47e07
bomin <rlaqhals0134@naver.com>
2021-08-02 16:24:35 +0900

    [S05P13C104-37]/FE_RoomUI : Add 방생성페이지 디자인 완료

--- frontend/src/components/CreateRoomModal.vue
+++ frontend/src/components/CreateRoomModal.vue
@@ -14,35 +14,56 @@
       <div class="row_box">
         <div class="left"></div>
         <div class="right">
-          <v-app id="inspire">
-                <v-container fluid>
+
+
+          <select class="card__input " >
+            <option >인원수</option>
+            <option value="volvo">2</option>
+            <option value="volvo">3</option>
+            <option value="volvo">4</option>
+            <option value="volvo">5</option>
+            <option value="volvo">6</option>
+          </select>
+
+          <!-- <v-app id="inspire">
+                <v-container >
                       <v-select
                         :items="items"
-                        filled
+                        
                         label="인원수"
+                        required
                       ></v-select>
                 </v-container>
-          </v-app>
+          </v-app> -->
 
         </div>
       </div>
 
-      <div class="row_box">
-            <v-card-text>
-              <v-row align="center">
-                <v-checkbox
-                  v-model="enabled"
-                  hide-details
-                  class="shrink mr-2 mt-0"
-                  label="비밀방"
-                ></v-checkbox>
-                <v-text-field
-                  :disabled="!enabled"
-                  label="I only work if you check the box"
-                ></v-text-field>
-              </v-row>
-            </v-card-text>
-      </div>
+        <div class="pw_box">
+          <v-checkbox
+            v-model="enabled"
+            hide-details
+            class="shrink mr-2 mt-0 "
+            label="비밀방"
+          ></v-checkbox>
+        </div>
+
+        <div v-if="enabled" >
+            <div class="row_box">
+                  <div class="row_box">
+                    <div class='left'></div>
+                    <div class='pw_right'>
+                      <v-text-field
+                        :disabled="!enabled"
+                        type=password
+                      ></v-text-field>
+                    </div>
+                  </div>
+            </div>
+        </div>
+
+  <!-- //////////////////////////////////////////////////////////////// -->
+       <button class="btn-animate">START</button>
       
     </form>
   </div>
@@ -50,6 +71,7 @@
 
 
 <script>
+
 export default {
   name: 'CreateRoomModal',
   data: function() {
@@ -57,6 +79,7 @@ export default {
         items: ['2','3','4','5','6'],
         includeFiles: true,
         enabled: false,
+
         contents: {
           title:'',
           password:'',
@@ -64,12 +87,16 @@ export default {
           gamecategory: '',
         },
        }
-    }
+    },
+  components: {
+    // Dropdown,
+  },
   }
 
 </script>
 
-<style scoped>
+
+<style scoped >
 
 .room_box {
   width: 38%;
@@ -100,288 +127,30 @@ export default {
 .right {
   height: 60px;
 }
-
-.left_pw {
+.pw_box {
+  width: 482px;
+  height: 30px;
+  /* background: rgba(219,219,219,0.6000000238418579); */
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.pw_right {
   height: 60px;
-}
-
-
-
-
-
-/* .card__input {
-
-
-
-
-
-
-
-
-.v362_53 {
-  width: 159px;
-  color: rgba(0,0,0,1);
-  position: absolute;
-  top: 27px;
-  left: 108px;
-  font-family: Roboto;
-  font-weight: Regular;
-  font-size: 30px;
-  opacity: 1;
-  text-align: left;
-}
-.v362_54 {
-  width: 515px;
-  height: 82px;
-  /* background: url("../images/v362_54.png"); */
-  /* background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 292px;
-  left: 217px;
-  overflow: hidden;
-} */
-.v362_55 {
+  padding: 0 15px;
   width: 405px;
-  height: 68px;
   background: rgba(219,219,219,0.6000000238418579);
-  opacity: 1;
-  position: absolute;
-  top: 8px;
-  left: 92px;
-  border: 1px solid rgba(219,219,219,0);
   border-top-right-radius: 20px;
   border-bottom-right-radius: 20px;
-  overflow: hidden;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
-.v362_56 {
-  width: 77px;
-  height: 68px;
-  background: rgba(49,46,46,0.5);
-  opacity: 1;
-  position: absolute;
-  top: 8px;
-  left: 15px;
-  border: 1px solid rgba(219,219,219,0.6000000238418579);
-  border-top-left-radius: 20px;
-  border-bottom-left-radius: 20px;
-  overflow: hidden;
+
+
+.pw_box /deep/ label {
+  color: rgb(194, 194, 194);
 }
-.v362_57 {
-  width: 159px;
-  color: rgba(0,0,0,1);
-  position: absolute;
-  top: 27px;
-  left: 108px;
-  font-family: Roboto;
-  font-weight: Regular;
-  font-size: 30px;
-  opacity: 1;
-  text-align: left;
-}
-.v362_58 {
-  width: 25px;
-  height: 19px;
-  background: rgba(196,196,196,1);
-  opacity: 1;
-  position: absolute;
-  top: 54px;
-  left: 65px;
-  transform: rotate(-178deg);
-}
-.v362_59 {
-  width: 515px;
-  height: 82px;
-  /* background: url("../images/v362_59.png"); */
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 292px;
-  left: 217px;
-  overflow: hidden;
-}
-.v362_60 {
-  width: 405px;
-  height: 68px;
-  background: rgba(219,219,219,0.6000000238418579);
-  opacity: 1;
-  position: absolute;
-  top: 8px;
-  left: 92px;
-  border: 1px solid rgba(219,219,219,0);
-  border-top-right-radius: 20px;
-  border-bottom-right-radius: 20px;
-  overflow: hidden;
-}
-.v362_61 {
-  width: 77px;
-  height: 68px;
-  background: rgba(49,46,46,0.5);
-  opacity: 1;
-  position: absolute;
-  top: 8px;
-  left: 15px;
-  border: 1px solid rgba(219,219,219,0.6000000238418579);
-  border-top-left-radius: 20px;
-  border-bottom-left-radius: 20px;
-  overflow: hidden;
-}
-.v362_62 {
-  width: 159px;
-  color: rgba(0,0,0,1);
-  position: absolute;
-  top: 27px;
-  left: 108px;
-  font-family: Roboto;
-  font-weight: Regular;
-  font-size: 30px;
-  opacity: 1;
-  text-align: left;
-}
-.v362_63 {
-  width: 25px;
-  height: 19px;
-  background: rgba(196,196,196,1);
-  opacity: 1;
-  position: absolute;
-  top: 54px;
-  left: 65px;
-  transform: rotate(-178deg);
-}
-.v362_64 {
-  width: 515px;
-  height: 82px;
-  /* background: url("../images/v362_64.png"); */
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 452px;
-  left: 219px;
-  overflow: hidden;
-}
-.v362_65 {
-  width: 405px;
-  height: 68px;
-  background: rgba(219,219,219,0.6000000238418579);
-  opacity: 1;
-  position: absolute;
-  top: 8px;
-  left: 92px;
-  border: 1px solid rgba(219,219,219,0);
-  border-top-right-radius: 20px;
-  border-bottom-right-radius: 20px;
-  overflow: hidden;
-}
-.v362_66 {
-  width: 77px;
-  height: 68px;
-  background: rgba(49,46,46,0.5);
-  opacity: 1;
-  position: absolute;
-  top: 8px;
-  left: 15px;
-  border: 1px solid rgba(219,219,219,0.6000000238418579);
-  border-top-left-radius: 20px;
-  border-bottom-left-radius: 20px;
-  overflow: hidden;
-}
-.v362_67 {
-  width: 159px;
-  color: rgba(0,0,0,1);
-  position: absolute;
-  top: 27px;
-  left: 108px;
-  font-family: Roboto;
-  font-weight: Regular;
-  font-size: 30px;
-  opacity: 1;
-  text-align: left;
-}
-.v362_68 {
-  width: 50px;
-  height: 46px;
-  /* background: url("../images/v362_68.png"); */
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 18px;
-  left: 29px;
-  overflow: hidden;
-}
-.v362_69 {
-  width: 128px;
-  color: rgba(255,255,255,1);
-  position: absolute;
-  top: 400px;
-  left: 261px;
-  font-family: Roboto;
-  font-weight: Regular;
-  font-size: 30px;
-  opacity: 1;
-  text-align: left;
-}
-.v362_70 {
-  width: 26px;
-  height: 28px;
-  background: rgba(196,196,196,1);
-  opacity: 1;
-  position: absolute;
-  top: 400px;
-  left: 219px;
-  overflow: hidden;
-}
-.v362_71 {
-  width: 605px;
-  height: 297px;
-  /* background: url("../images/v362_71.png"); */
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 30px;
-  left: 175px;
-  overflow: hidden;
-}
-.v362_72 {
-  width: 345px;
-  height: 68px;
-  background: rgba(196,196,196,0.30000001192092896);
-  opacity: 1;
-  position: absolute;
-  top: 557px;
-  left: 305px;
-  border: 1px solid rgba(255,250,250,1);
-  border-top-left-radius: 20px;
-  border-top-right-radius: 20px;
-  border-bottom-left-radius: 20px;
-  border-bottom-right-radius: 20px;
-  overflow: hidden;
-}
-.v362_73 {
-  width: 108px;
-  color: rgba(0,0,0,1);
-  position: absolute;
-  top: 570px;
-  left: 421px;
-  font-family: Roboto;
-  font-weight: Regular;
-  font-size: 36px;
-  opacity: 1;
-  text-align: center;
-}
-.name {
-  color: #fff;
-}
-.name {
+
+.pw_box /deep/ v-checkbox{
   color: #fff;
 }
 
