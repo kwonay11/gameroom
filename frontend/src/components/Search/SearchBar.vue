a47767771817e35f1f64b217398a04c5af4b73a0
bomin <rlaqhals0134@naver.com>
2021-08-10 01:05:36 +0900

    [S05P13C104-30] feat/fe_search : Add 검색 기능 구현 완료

--- frontend/src/components/Search/SearchBar.vue
+++ frontend/src/components/Search/SearchBar.vue
@@ -1,10 +1,16 @@
 <template>
+<div>
   <div>
-    <div class="search-bar" :style="{ margin: GameLength ? '2vh auto 2vh' : '18vh auto 6vh' }">
-      <p>방 제목(공백 제거 후 처리) 혹은 방장 닉네임 검색</p>
-      <input v-model='search_input' placeholder="search" @keyup.enter='SearchKeyword' type="text" />
-    </div>
+      <!-- <p>방 제목(공백 제거 후 처리) 혹은 방장 닉네임 검색</p> -->
+    <div class="search-bar" :style="{ margin: GameLength ? '2vh auto 2vh' : '18vh auto 6vh' }" > 
+      <img class="image" src="@/assets/loupe (4).png" alt="search"/>
+      <input v-model='search_input' placeholder="Search" @keyup.enter='SearchKeyword' type="text" class="inner_search" />
+      <!-- <v-text-field v-model='search_input' placeholder="search" @keyup.enter='SearchKeyword' :rules="rules" class="inner_search"></v-text-field> -->
   </div>
+
+  </div>
+  </div>
+
 </template>
 <script>
 
@@ -18,6 +24,10 @@ export default {
     return {
       search_input: '',
       GameLength: null,
+      rules: [
+        value => !!value ,
+        value => (value || '').length <= 20 || 'Max 20 characters',
+      ],
     }
   },
 
@@ -42,17 +52,34 @@ export default {
 
 
 <style >
+/*  */
 .search-bar {
   /* transition: margin 0.5s; */
   text-align: center;
   width: 80%;
+  background-color: rgba(20, 17, 151, 0.4);
+  border-radius: 20px;
+  padding: 1%;
 
 }
 
-.search-bar > input {
-  width: 100%;      /* 가로 너비 배치를 화면의 끝과 끝으로 배치 */
-  padding: 0.5rem;  /* input 안쪽의 margin을 여유롭게 */
+.inner_search {
+  width: 90%;      /* 가로 너비 배치를 화면의 끝과 끝으로 배치 */
+  padding: 0.7rem 1rem;  /* input 안쪽의 margin을 여유롭게 */
   font-size: 2rem;
-  background-color: red;
+  background-color: white;
+  border-radius: 15px;
+  border:none
+  
 }
+.image{
+  width:4%;
+  /* height:50%; */
+  padding-bottom: 0.5%;
+  margin-right:2.5%;
+}
+input:focus {
+  outline: none;
+  box-shadow: 0px 0px 20px rgb(2, 1, 12);
+  }
 </style>
