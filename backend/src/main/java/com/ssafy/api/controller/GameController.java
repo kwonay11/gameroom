d46c92d6f38c10eb3400a2f301b9f8863cfcd87d
정동원 <dongwoncheong@gmail.com>
2021-08-11 17:45:51 +0900

    [] feat/BE_GameRoomInfo : Add 방정보API작성

--- backend/src/main/java/com/ssafy/api/controller/GameController.java
+++ backend/src/main/java/com/ssafy/api/controller/GameController.java
@@ -1,6 +1,9 @@
 package com.ssafy.api.controller;
-import com.ssafy.api.service.GameCategoryService;
-import com.ssafy.db.entity.*;
+
+import com.ssafy.api.request.GameStatusGetReq;
+import com.ssafy.api.response.GameStatusRes;
+import com.ssafy.api.service.GameService;
+import com.ssafy.db.entity.GameCategory;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -10,25 +13,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
-/**
- * 유저 관련 API 요청 처리를 위한 컨트롤러 정의.
- */
-@Api(value = "게임 API", tags = {"Games"})
+@Api(value = "게임 API", tags = {"Game"})
+
 @RestController
 @RequestMapping("/api/games")
-public class GameCategoryController {
+public class GameController {
 
     @Autowired
-    GameCategoryService gameService;
+    GameService gameService;
 
     @GetMapping()
     @ApiOperation(value = "게임 이름과 설명 조회", notes = "게임 이름과 게임 설명을 response함")
     @ApiResponses({
             @ApiResponse(code = 200, message = "성공"),
-            @ApiResponse(code = 401, message = "인증 실패"),
-            @ApiResponse(code = 404, message = "사용자 없음"),
             @ApiResponse(code = 500, message = "서버 오류")
     })
     public ResponseEntity<List<GameCategory>> getGameSummary() {
@@ -36,6 +36,25 @@ public class GameCategoryController {
          * game_category 테이블에서 게임 이름과 게임 설명을 조회하여 response 해줌
          */
         List<GameCategory> res = gameService.getGameCategoryList();
+
+        return ResponseEntity.status(200).body(res);
+    }
+
+
+    @GetMapping("/play")
+    @ApiOperation(value = "게임 진행", notes = "게임이 진행되면서 필요한 데이터를 주고 받음")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<?> getGameStatus(GameStatusGetReq gameStatusGetReq) {
+
+
+        GameStatusRes res = GameStatusRes.builder()
+                                        .keyword("")
+                                        .questioner(0L)
+                                        .round(gameStatusGetReq.getRound() + 1)
+                                        .build();
 
         return ResponseEntity.status(200).body(res);
     }
