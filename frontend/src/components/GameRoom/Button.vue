1b9e54599dfe92453e6e5cd4e7dbeb831525dce0
bomin <rlaqhals0134@naver.com>
2021-08-11 15:33:52 +0900

    [S05P13C104-11] feat/fe_button : Add 링크 복사 버튼 구현 완료

--- frontend/src/components/GameRoom/Button.vue
+++ frontend/src/components/GameRoom/Button.vue
@@ -4,8 +4,16 @@
         <!-- 방장만 보이게 하기 -->
         <img  src="@/assets/settings.png" alt="setting">
 
-        <img  src="@/assets/link.png" alt="link">
+        <img  src="@/assets/link.png" alt="link" @click='link'>
       </div>
+    <app-my-modal :visible.sync="visible">
+      <div>
+        <input id='url' class="_input" v-model="url"  type="text"/>
+        <br>
+        <button class="btn-animate" @click="doCopy()">copy</button>        <!-- <button class="btn-animate">확인</button> -->
+
+      </div>
+    </app-my-modal>
 
       <div class='control'>
             <!-- 음소거 -->
@@ -40,8 +48,21 @@
   </div>
 </template>
 <script>
+import myModal from '@/components/myModal'
+import swal from 'sweetalert';
+
 export default {
   name: 'Button',
+
+  data() {
+    return {
+       visible: false,
+       url: null,
+    }
+  },
+  components: {
+    appMyModal: myModal,
+  },
 
   props:{
     publisher: Object,
@@ -52,21 +73,31 @@ export default {
     muteAudio() {
       if (this.publisher.stream.audioActive) {
         this.publisher.publishAudio(false)
-      }
-      else {
+      }else {
         this.publisher.publishAudio(true)
       }
-
     },
     muteVideo() {
       if (this.publisher.stream.videoActive) {
         this.publisher.publishVideo(false)
-      }
-      else {
+      }else {
         this.publisher.publishVideo(true)
       }
-
-    }
+    },
+    link() {
+      this.visible = !this.visible
+      
+      // const urlInput = document.getElementById('urlInput');
+      this.url = window.document.location.href
+    },
+    doCopy() {
+      const my_url = document.getElementById('url')
+      my_url.select();
+      document.execCommand('copy')
+      swal(`링크 복사 완료!`)
+      this.visible = false
+      
+    },
   }
 
   
