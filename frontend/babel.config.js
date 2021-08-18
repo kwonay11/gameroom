4f3d42901d176f59e30aeffc823a44714b82e95a
Yunbin Kim <bin6246@naver.com>
2021-08-18 23:45:56 +0900

    hotfix / hotfix : Add 배포용 콘솔 로그지우기

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,5 +1,11 @@
+const removeConsolePlugin = ['@vue/cli-plugin-babel/preset']
+if(process.env.NODE_ENV === 'production') {
+  removeConsolePlugin.push("transform-remove-console")
+}
+
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+    '@vue/app'
+  ],
+  plugins: removeConsolePlugin
 }
